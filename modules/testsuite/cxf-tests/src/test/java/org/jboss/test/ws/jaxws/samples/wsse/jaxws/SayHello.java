9c6dd570971cd022854efa97b02107a5ecc78063
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2008-05-29 15:45:01 +0000

    [JBWS-2098] Adding signature test case

---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsse/jaxws/SayHello.java
@@ -0,0 +1,15 @@
+
+package org.jboss.test.ws.jaxws.samples.wsse.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "sayHello", namespace = "http://www.jboss.org/jbossws/ws-extensions/wssecurity")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "sayHello", namespace = "http://www.jboss.org/jbossws/ws-extensions/wssecurity")
+public class SayHello {
+
+
+}
---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsse/jaxws/SayHelloResponse.java
@@ -0,0 +1,36 @@
+
+package org.jboss.test.ws.jaxws.samples.wsse.jaxws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "sayHelloResponse", namespace = "http://www.jboss.org/jbossws/ws-extensions/wssecurity")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "sayHelloResponse", namespace = "http://www.jboss.org/jbossws/ws-extensions/wssecurity")
+public class SayHelloResponse {
+
+    @XmlElement(name = "return", namespace = "")
+    private String _return;
+
+    /**
+     * 
+     * @return
+     *     returns String
+     */
+    public String getReturn() {
+        return this._return;
+    }
+
+    /**
+     * 
+     * @param _return
+     *     the value for the _return property
+     */
+    public void setReturn(String _return) {
+        this._return = _return;
+    }
+
+}
