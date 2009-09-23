636487dff6309c2858b158898f91c6b1a15a252c
jim.ma <jim.ma>
2009-09-23 03:26:05 +0000

    [JBWS-2760]:make the windows bat scrip when JAVA_HOME not set

--- modules/resources/src/main/resources/bin/wsprovide.bat
+++ modules/resources/src/main/resources/bin/wsprovide.bat
@@ -8,12 +8,21 @@ if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
 set PROGNAME=run.bat
 if "%OS%" == "Windows_NT" set PROGNAME=%~nx0%
 
-set JAVA=%JAVA_HOME%\bin\java
+if "x%JAVA_HOME%" == "x" (
+  set  JAVA=java
+  echo JAVA_HOME is not set. Unexpected results may occur.
+  echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) else (
+  set "JAVA=%JAVA_HOME%\bin\java"
+  if exist "%JAVA_HOME%\lib\tools.jar" (
+    set "JAVAC_JAR=%JAVA_HOME%\lib\tools.jar"
+  )
+)
 set JBOSS_HOME=%DIRNAME%\..
 set JBOSS_ENDORSED_DIRS=%JBOSS_HOME%\lib\endorsed
 
 rem shared libs
-set WSPROVIDE_CLASSPATH=%WSPROVIDE_CLASSPATH%;%JAVA_HOME%/lib/tools.jar
+set WSPROVIDE_CLASSPATH=%WSPROVIDE_CLASSPATH%;%JAVAC_JAR%
 set WSPROVIDE_CLASSPATH=%WSPROVIDE_CLASSPATH%;%JBOSS_HOME%/client/jbossws-spi.jar
 set WSPROVIDE_CLASSPATH=%WSPROVIDE_CLASSPATH%;%JBOSS_HOME%/client/jbossws-common.jar
 set WSPROVIDE_CLASSPATH=%WSPROVIDE_CLASSPATH%;%JBOSS_HOME%/client/jbossws-framework.jar
--- modules/resources/src/main/resources/bin/wsrunclient.bat
+++ modules/resources/src/main/resources/bin/wsrunclient.bat
@@ -31,7 +31,14 @@ if [%1] == [] goto endloop
       goto loop
 :endloop
 
-set JAVA=%JAVA_HOME%\bin\java
+if "x%JAVA_HOME%" == "x" (
+  set  JAVA=java
+  echo JAVA_HOME is not set. Unexpected results may occur.
+  echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) else (
+  set "JAVA=%JAVA_HOME%\bin\java"
+)
+
 set JBOSS_HOME=%DIRNAME%\..
 set JBOSS_ENDORSED_DIRS=%JBOSS_HOME%\lib\endorsed
 
