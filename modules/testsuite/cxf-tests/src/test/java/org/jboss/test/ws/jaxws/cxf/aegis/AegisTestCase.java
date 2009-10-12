cb87a413733e91d3e557bafc321331d5326bb1b2
jim.ma <jim.ma>
2009-10-12 03:02:36 +0000

    [JBWS-2766]:jaxws aegis databinding test case

---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/aegis/AegisTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.cxf.aegis;
+
+import java.net.URL;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import junit.framework.Test;
+
+import org.apache.cxf.aegis.databinding.AegisDatabinding;
+import org.apache.cxf.frontend.ClientProxyFactoryBean;
+import org.jboss.test.ws.jaxws.cxf.descriptor.DescriptorEndpoint;
+import org.jboss.test.ws.jaxws.cxf.descriptor.DescriptorJSETestCase;
+import org.jboss.wsf.test.JBossWSTest;
+import org.jboss.wsf.test.JBossWSTestSetup;
+
+public class AegisTestCase extends JBossWSTest
+{
+   private String endpointURL = "http://" + getServerHost() + ":9090/jaxws-aegis";
+   private String targetNS = "http://org.jboss.ws.jaxws.cxf/descriptor";
+
+   public static Test suite()
+   {
+      return new JBossWSTestSetup(AegisTestCase.class, "jaxws-aegis.war");
+   }
+
+   public void testLegalAccess() throws Exception
+   {
+      ClientProxyFactoryBean proxyFactory = new ClientProxyFactoryBean();
+      proxyFactory.setDataBinding(new AegisDatabinding());
+      proxyFactory.setServiceClass(AegisGroupQuery.class);
+      proxyFactory.setAddress(endpointURL);
+      AegisGroupQuery query = (AegisGroupQuery)proxyFactory.create();
+      Map<Integer, String> members =  query.getMembers();
+      this.assertEquals(2, members.size());
+      this.assertEquals(true, members.containsKey(2));
+   }
+
+}
