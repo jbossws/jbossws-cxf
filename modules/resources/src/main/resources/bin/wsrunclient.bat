cc74a026a95295beff4ab9acc161d1cdc4a1769d
jim.ma <jim.ma>
2009-10-13 08:13:52 +0000

    [JBWS-2760]:Merge the fix to branch

--- modules/resources/src/main/resources/bin/wsrunclient.bat
+++ modules/resources/src/main/resources/bin/wsrunclient.bat
@@ -31,7 +31,16 @@ if [%1] == [] goto endloop
       goto loop
 :endloop
 
-set JAVA=%JAVA_HOME%\bin\java
+if "x%JAVA_HOME%" == "x" (
+  set  JAVA=java
+  echo JAVA_HOME is not set. Unexpected results may occur.
+  echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+) else (
+  set "JAVA=%JAVA_HOME%\bin\java"
+  if exist "%JAVA_HOME%\lib\tools.jar" (
+    set "JAVAC_JAR=%JAVA_HOME%\lib\tools.jar"
+  )
+)
 set JBOSS_HOME=%DIRNAME%\..
 set JBOSS_ENDORSED_DIRS=%JBOSS_HOME%\lib\endorsed
 
