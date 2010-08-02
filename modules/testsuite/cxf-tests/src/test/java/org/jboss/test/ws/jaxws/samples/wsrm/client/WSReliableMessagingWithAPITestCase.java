e7039bfb3c2a395b6666daa9433f0b8895d52e68
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2010-08-02 16:10:30 +0000

    [JBWS-3085] Adding testcase for WS-RM usage through policy / api only

---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsrm/client/WSReliableMessagingWithAPITestCase.java
@@ -0,0 +1,68 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.samples.wsrm.client;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import junit.framework.Test;
+
+import org.jboss.test.ws.jaxws.samples.wsrm.generated.SimpleService;
+import org.jboss.wsf.test.JBossWSTest;
+import org.jboss.wsf.test.JBossWSTestSetup;
+
+/**
+ * Client invoking web service with WS-RM and using no xml descriptor
+ *
+ * @author alessio.soldano@jboss.com
+ * @since 02-Aug-2010
+ * 
+ */
+public final class WSReliableMessagingWithAPITestCase extends JBossWSTest
+{
+   private final String serviceURL = "http://" + getServerHost() + ":8080/jaxws-samples-wsrm-api/SimpleService";
+   private SimpleService proxy;
+   
+   public static Test suite()
+   {
+      return new JBossWSTestSetup(WSReliableMessagingWithAPITestCase.class, "jaxws-samples-wsrm-api.war");
+   }
+
+   @Override
+   protected void setUp() throws Exception
+   {
+      super.setUp();
+      QName serviceName = new QName("http://www.jboss.org/jbossws/ws-extensions/wsrm", "SimpleService");
+      URL wsdlURL = new URL(serviceURL + "?wsdl");
+      Service service = Service.create(wsdlURL, serviceName);
+      proxy = (SimpleService)service.getPort(SimpleService.class);
+   }
+
+   public void test() throws Exception
+   {
+      proxy.ping(); // one way call
+      assertEquals("Hello World!", proxy.echo("Hello World!")); // request response call
+   }
+   
+}
---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsrm/service/RMCheckInterceptor.java
@@ -0,0 +1,78 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.samples.wsrm.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.io.CachedOutputStream;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+/**
+ * A server side interceptor that verifies every incoming message has WS-RM stuff
+ * (meaning the client successfully understood server's policy enforcing ws-rm)
+ *
+ * @author alessio.soldano@jboss.com
+ * @since 02-Aug-2010
+ */
+public class RMCheckInterceptor extends AbstractPhaseInterceptor<Message>
+{
+   private static final String RM_NS = "http://schemas.xmlsoap.org/ws/2005/02/rm";
+   
+   public RMCheckInterceptor()
+   {
+      super(Phase.RECEIVE);
+   }
+   
+   public void handleMessage(Message message) throws Fault
+   {
+      StringBuilder sb = new StringBuilder();
+      InputStream is = message.getContent(InputStream.class);
+      if (is != null)
+      {
+         CachedOutputStream bos = new CachedOutputStream();
+         try
+         {
+            IOUtils.copy(is, bos);
+            bos.flush();
+            is.close();
+            message.setContent(InputStream.class, bos.getInputStream());
+            bos.writeCacheTo(sb);
+            bos.close();
+         }
+         catch (IOException e)
+         {
+            throw new Fault(e);
+         }
+      }
+      if (!sb.toString().contains(RM_NS))
+      {
+         throw new Fault("Could not find any reference to namespace '" + RM_NS + "' in handled message.",
+               java.util.logging.Logger.getLogger(RMCheckInterceptor.class.getName()));
+      }
+   }
+
+}
