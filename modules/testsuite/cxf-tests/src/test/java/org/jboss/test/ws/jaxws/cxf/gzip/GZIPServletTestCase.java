f39e67b627fc75f2a9eec8efcc5375d4031b886e
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2011-04-01 16:19:57 +0000

    Adding an in-container servlet-client-based version of gzip testcase, refactoring the test a bit and fixing an issue with erroneusly re-using HTTPConduit

--- modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/gzip/GZIPServletTestCase.java
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/gzip/GZIPServletTestCase.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -21,64 +21,65 @@
  */
 package org.jboss.test.ws.jaxws.cxf.gzip;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import junit.framework.Test;
 
 import org.jboss.wsf.test.JBossWSCXFTestSetup;
 import org.jboss.wsf.test.JBossWSTest;
 
 /**
+ * This covers the same tests of GZIPTestCase but runs them via a Servlet client
  * 
  * @author alessio.soldano@jboss.com
- * @since 15-Sep-2010
+ * @since 01-Apr-2011
  *
  */
-public class GZIPTestCase extends JBossWSTest
+public class GZIPServletTestCase extends JBossWSTest
 {
-   private String gzipFeatureEndpointURL = "http://" + getServerHost() + ":8080/jaxws-cxf-gzip/HelloWorldService/HelloWorldImpl";
-   
-   private Helper helper;
-
    public static Test suite()
    {
-      return new JBossWSCXFTestSetup(GZIPTestCase.class, "jaxws-cxf-gzip.war");
-   }
-   
-   private Helper getHelper()
-   {
-      if (helper == null)
-      {
-         helper = new Helper(gzipFeatureEndpointURL);
-      }
-      return helper;
+      return new JBossWSCXFTestSetup(GZIPServletTestCase.class, "jaxws-cxf-gzip.war, jaxws-cxf-gzip-client.war");
    }
    
    public void testGZIPUsingFeatureOnBus() throws Exception
    {
-      assertTrue(getHelper().testGZIPUsingFeatureOnBus());
+      assertEquals("1", runTestInContainer("testGZIPUsingFeatureOnBus"));
    }
    
    public void testGZIPUsingFeatureOnClient() throws Exception
    {
-      assertTrue(getHelper().testGZIPUsingFeatureOnClient());
+      assertEquals("1", runTestInContainer("testGZIPUsingFeatureOnClient"));
    }
    
    public void testGZIPServerSideOnlyInterceptorOnClient() throws Exception
    {
-      assertTrue(getHelper().testGZIPServerSideOnlyInterceptorOnClient());
+      assertEquals("1", runTestInContainer("testGZIPServerSideOnlyInterceptorOnClient"));
    }
    
    public void testFailureGZIPServerSideOnlyInterceptorOnClient() throws Exception
    {
-      assertTrue(getHelper().testFailureGZIPServerSideOnlyInterceptorOnClient());
+      assertEquals("1", runTestInContainer("testFailureGZIPServerSideOnlyInterceptorOnClient"));
    }
    
    public void testGZIPServerSideOnlyInterceptorsOnBus() throws Exception
    {
-      assertTrue(getHelper().testGZIPServerSideOnlyInterceptorsOnBus());
+      assertEquals("1", runTestInContainer("testGZIPServerSideOnlyInterceptorsOnBus"));
    }
 
    public void testFailureGZIPServerSideOnlyInterceptorsOnBus() throws Exception
    {
-      assertTrue(getHelper().testFailureGZIPServerSideOnlyInterceptorsOnBus());
+      assertEquals("1", runTestInContainer("testFailureGZIPServerSideOnlyInterceptorsOnBus"));
+   }
+   
+   private String runTestInContainer(String test) throws Exception
+   {
+      URL url = new URL("http://" + getServerHost()
+            + ":8080/jaxws-cxf-gzip-client?path=/jaxws-cxf-gzip/HelloWorldService/HelloWorldImpl&method=" + test
+            + "&helper=" + Helper.class.getName());
+      BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+      return br.readLine();
    }
 }
---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/gzip/Helper.java
@@ -0,0 +1,180 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.cxf.gzip;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transport.http.gzip.GZIPFeature;
+import org.apache.cxf.transport.http.gzip.GZIPOutInterceptor;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
+import org.jboss.wsf.test.ClientHelper;
+
+public class Helper implements ClientHelper
+{
+   private String gzipFeatureEndpointURL;
+   
+   public Helper()
+   {
+      
+   }
+
+   public Helper(String endpointURL)
+   {
+      setTargetEndpoint(endpointURL);
+   }
+
+   public boolean testGZIPUsingFeatureOnBus() throws Exception
+   {
+      Bus bus = BusFactory.newInstance().createBus();
+      try
+      {
+         BusFactory.setThreadDefaultBus(bus);
+         
+         GZIPFeature gzipFeature = new GZIPFeature();
+         gzipFeature.setThreshold(0);
+         gzipFeature.initialize(bus);
+
+         HelloWorld port = getPort();
+         return ("foo".equals(port.echo("foo")));
+      }
+      finally
+      {
+         bus.shutdown(true);
+      }
+   }
+   
+   public boolean testGZIPUsingFeatureOnClient() throws Exception
+   {
+      HelloWorld port = getPort();
+      Client client = ClientProxy.getClient(port);
+      GZIPFeature gzipFeature = new GZIPFeature();
+      gzipFeature.setThreshold(0);
+      gzipFeature.initialize(client, BusFactory.getThreadDefaultBus());
+      return ("foo".equals(port.echo("foo")));
+   }
+   
+   public boolean testGZIPServerSideOnlyInterceptorOnClient() throws Exception
+   {
+      HelloWorld port = getPort();
+      Client client = ClientProxy.getClient(port);
+      HTTPConduit conduit = (HTTPConduit)client.getConduit();
+      HTTPClientPolicy policy = conduit.getClient();
+      //enable Accept gzip, otherwise the server will not try to reply using gzip
+      policy.setAcceptEncoding("gzip;q=1.0, identity; q=0.5, *;q=0");
+      //add interceptor for decoding gzip message
+      client.getInInterceptors().add(new GZIPEnforcingInInterceptor());
+      return ("foo".equals(port.echo("foo")));
+   }
+   
+   public boolean testFailureGZIPServerSideOnlyInterceptorOnClient() throws Exception
+   {
+      HelloWorld port = getPort();
+      Client client = ClientProxy.getClient(port);
+      HTTPConduit conduit = (HTTPConduit)client.getConduit();
+      HTTPClientPolicy policy = conduit.getClient();
+      //enable Accept gzip, otherwise the server will not try to reply using gzip
+      policy.setAcceptEncoding("gzip;q=1.0, identity; q=0.5, *;q=0");
+      try
+      {
+         port.echo("foo");
+         return false;
+      }
+      catch (Exception e)
+      {
+         //expected exception, as the client is not able to decode gzip message
+         return true;
+      }
+   }
+   
+   public boolean testGZIPServerSideOnlyInterceptorsOnBus() throws Exception
+   {
+      Bus bus = BusFactory.newInstance().createBus();
+      try
+      {
+         //enable Accept gzip, otherwise the server will not try to reply using gzip
+         GZIPOutInterceptor outInterceptor = new GZIPOutInterceptor();
+         outInterceptor.setThreshold(1024*1024); // 1MB -> no gzip on request
+         bus.getOutInterceptors().add(outInterceptor);
+         //add interceptor for decoding gzip message
+         bus.getInInterceptors().add(new GZIPEnforcingInInterceptor());
+         BusFactory.setThreadDefaultBus(bus);
+         HelloWorld port = getPort();
+         return ("foo".equals(port.echo("foo")));
+      }
+      finally
+      {
+         bus.shutdown(true);
+      }
+   }
+
+   public boolean testFailureGZIPServerSideOnlyInterceptorsOnBus() throws Exception
+   {
+      Bus bus = BusFactory.newInstance().createBus();
+      try
+      {
+         //enable Accept gzip, otherwise the server will not try to reply using gzip
+         GZIPOutInterceptor outInterceptor = new GZIPOutInterceptor();
+         outInterceptor.setThreshold(1024*1024); // 1MB -> no gzip on request
+         bus.getOutInterceptors().add(outInterceptor);
+         BusFactory.setThreadDefaultBus(bus);
+         HelloWorld port = getPort();
+         try
+         {
+            port.echo("foo");
+            return false;
+         }
+         catch (Exception e)
+         {
+            //expected exception, as the client is not able to decode gzip message
+            return true;
+         }
+      }
+      finally
+      {
+         bus.shutdown(true);
+      }
+   }
+   
+   private HelloWorld getPort() throws MalformedURLException
+   {
+      URL wsdlURL = new URL(gzipFeatureEndpointURL + "?wsdl");
+      QName serviceName = new QName("http://org.jboss.ws/jaxws/cxf/gzip", "HelloWorldService");
+      Service service = Service.create(wsdlURL, serviceName);
+      QName portQName = new QName("http://org.jboss.ws/jaxws/cxf/gzip", "HelloWorldImplPort");
+      return (HelloWorld) service.getPort(portQName, HelloWorld.class);
+   }
+
+   @Override
+   public void setTargetEndpoint(String address)
+   {
+      this.gzipFeatureEndpointURL = address;
+   }
+}
