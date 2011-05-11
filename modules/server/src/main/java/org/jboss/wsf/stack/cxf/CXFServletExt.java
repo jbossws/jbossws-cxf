5c848ea57e909816acf6ad5624ae430349c29096
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2011-05-11 16:52:29 +0000

    [JBWS-3279] Unifying CXFServletExt

--- modules/server/src/main/java/org/jboss/wsf/stack/cxf/CXFServletExt.java
+++ modules/server/src/main/java/org/jboss/wsf/stack/cxf/CXFServletExt.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -21,7 +21,23 @@
  */
 package org.jboss.wsf.stack.cxf;
 
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.resource.ResourceManager;
+import org.apache.cxf.transport.servlet.AbstractHTTPServlet;
+import org.apache.cxf.transport.servlet.ServletContextResourceResolver;
+import org.jboss.wsf.spi.deployment.Endpoint;
 import org.jboss.wsf.spi.deployment.ServletDelegate;
+import org.jboss.wsf.stack.cxf.configuration.BusHolder;
+import org.jboss.wsf.stack.cxf.transport.ServletHelper;
 
 /**
  * An extension to the CXF servlet
@@ -31,7 +47,91 @@ import org.jboss.wsf.spi.deployment.ServletDelegate;
  * 
  * @since 21-Apr-2007
  */
-public class CXFServletExt extends CXFNonSpringServletExt implements ServletDelegate
+public class CXFServletExt extends AbstractHTTPServlet implements ServletDelegate
 {
+   protected Endpoint endpoint;
+   protected Bus bus;
 
+   @Override
+   public void init(ServletConfig sc) throws ServletException {
+       super.init(sc);
+       loadBus(sc);
+   }
+
+   protected void loadBus(ServletConfig servletConfig) throws ServletException
+   {
+      //Init the Endpoint
+      endpoint = ServletHelper.initEndpoint(servletConfig, getServletName());
+
+      //keep the bus created during deployment and update it with the information coming from the servlet config
+      updateAvailableBusWithServletInfo(servletConfig);
+
+      //register the InstrumentManagementImpl
+      ServletHelper.registerInstrumentManger(bus, getServletContext());
+   }
+
+   private void updateAvailableBusWithServletInfo(ServletConfig servletConfig)
+   {
+      BusHolder holder = endpoint.getService().getDeployment().getAttachment(BusHolder.class);
+      //set the bus from deployment into the CXF servlet and assign it to the current thread (do not touch the default bus!)
+      bus = holder.getBus();
+      BusFactory.setThreadDefaultBus(bus);
+      //update the resource manager adding the ServletContextResourceResolver that was to be added by CXF servlet
+      ResourceManager resourceManager = bus.getExtension(ResourceManager.class);
+      resourceManager.addResourceResolver(new ServletContextResourceResolver(servletConfig.getServletContext()));
+   }
+
+   @Override
+   protected void invoke(HttpServletRequest req, HttpServletResponse res) throws ServletException
+   {
+      ServletHelper.callRequestHandler(req, res, getServletContext(), bus, endpoint);
+   }
+   
+   @Override
+   public void destroy() 
+   {
+      ServletHelper.callPreDestroy(endpoint);
+   }
+   
+   @Override
+   public void doHead(HttpServletRequest request, HttpServletResponse response, ServletContext context)
+         throws ServletException, IOException
+   {
+      this.doHead(request, response);
+   }
+
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response, ServletContext context)
+         throws ServletException, IOException
+   {
+      this.doGet(request, response);
+   }
+
+   @Override
+   public void doPost(HttpServletRequest request, HttpServletResponse response, ServletContext context)
+         throws ServletException, IOException
+   {
+      this.doPost(request, response);
+   }
+
+   @Override
+   public void doPut(HttpServletRequest request, HttpServletResponse response, ServletContext context)
+         throws ServletException, IOException
+   {
+      this.doPut(request, response);
+   }
+
+   @Override
+   public void doDelete(HttpServletRequest request, HttpServletResponse response, ServletContext context)
+         throws ServletException, IOException
+   {
+      this.doDelete(request, response);
+   }
+
+   @Override
+   public void service(HttpServletRequest request, HttpServletResponse response, ServletContext context)
+         throws ServletException, IOException
+   {
+      this.service(request, response);
+   }
 }
