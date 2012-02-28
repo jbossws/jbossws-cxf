4f4b9f99e2c1e65db7ddeeb75a9fe28ea2ce9a9a
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2012-02-28 11:04:49 +0000

    [JBWS-2710][JBWS-3434][JBWS-2639] Removing MSFT endpoint based interop tests, which were there for the sake of testing WS-Trust client support only; this is being replaced by the tests for Apache CXF STS

--- modules/testsuite/cxf-spring-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/interop/wstrust10/org/xmlsoap/ping/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xmlsoap.org/Ping", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.jboss.test.ws.jaxws.cxf.interop.wstrust10.org.xmlsoap.ping;
