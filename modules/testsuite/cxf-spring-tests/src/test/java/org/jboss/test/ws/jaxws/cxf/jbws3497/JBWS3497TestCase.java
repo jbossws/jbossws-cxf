057de590b3e47444f39aa066067de84faef14aa8
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2012-05-16 15:00:53 +0000

    [JBWS-3497] Adding testcase for workaround

---
+++ modules/testsuite/cxf-spring-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/jbws3497/JBWS3497TestCase.java
@@ -0,0 +1,139 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.cxf.jbws3497;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import junit.framework.Test;
+
+import org.jboss.wsf.test.JBossWSCXFTestSetup;
+import org.jboss.wsf.test.JBossWSTest;
+
+/**
+ * [JBWS-3497] Add ability to configure the queue depth on the asynchronous (@Oneway) work queue.
+ * This testcase basically verifies the initial workaround for the issue works.
+ * 
+ * @author alessio.soldano@jboss.com
+ *
+ */
+public class JBWS3497TestCase extends JBossWSTest
+{
+   private String endpointOneURL = "http://" + getServerHost() + ":8080/jaxws-cxf-jbws3497/ServiceOne/EndpointOne";
+   private String targetNS = "http://org.jboss.ws.jaxws.cxf/jbws3497";
+   
+   private EndpointOne portOne;
+   
+   protected int defaultSize = 200;
+   
+
+   public static Test suite()
+   {
+      return new JBossWSCXFTestSetup(JBWS3497TestCase.class, "jaxws-cxf-jbws3497.war");
+   }
+   
+   public void testAccess() throws Exception
+   {
+      initPorts();
+      int count1 = portOne.getCount();
+      Object retObj = portOne.echo("Hello");
+      assertEquals("Hello", retObj);
+      assertEquals(1, portOne.getCount() - count1);
+   }
+   
+   //Disabled as there's no easy way for knowing the server is logging WARN messages saying the @OneWay processing queue is full.
+   //Manually enable this test if willing to check (and try modifying 'queueSize' value in cxf.xml); otherwise the EndpointOne
+   //implementation simply checks the related AutomaticWorkQueue is properly configured. 
+   public void _testConcurrentOneWayInvocations() throws Exception
+   {
+      runConcurrentTests(true);
+   }
+   
+   private void runConcurrentTests(boolean oneway) throws Exception
+   {
+      initPorts();
+      final int size = defaultSize;
+      int count1 = portOne.getCount();
+      ExecutorService es = Executors.newFixedThreadPool(size);
+      List<Callable<Boolean>> callables = new ArrayList<Callable<Boolean>>(size*3);
+      for (int i = 0; i < size*3; i++)
+      {
+         callables.add(new CallableOne(portOne, oneway, i));
+      }
+      List<Future<Boolean>> futures = es.invokeAll(callables);
+      for (Future<Boolean> f : futures)
+      {
+         assertTrue(f.get());
+      }
+      if (oneway) {
+         Thread.sleep(3000);
+      }
+      assertEquals(size*3, portOne.getCount() - count1);
+   }
+   
+   private void initPorts() throws MalformedURLException
+   {
+      URL wsdlOneURL = new URL(endpointOneURL + "?wsdl");
+      QName serviceOneName = new QName(targetNS, "ServiceOne");
+      Service serviceOne = Service.create(wsdlOneURL, serviceOneName);
+      portOne = (EndpointOne)serviceOne.getPort(EndpointOne.class);
+   }
+   
+   private static class CallableOne implements Callable<Boolean>
+   {
+      private EndpointOne port;
+      private boolean oneway;
+      private int seqNum;
+      
+      public CallableOne(EndpointOne port, boolean oneway, int seqNum)
+      {
+         this.port = port;
+         this.oneway = oneway;
+         this.seqNum = seqNum;
+      }
+      
+      public Boolean call() throws Exception
+      {
+         String arg = "Foo" + seqNum;
+         if (oneway)
+         {
+            port.echoOneWay(arg);
+            return true;
+         }
+         else
+         {
+            String result = port.echo(arg);
+            return arg.equals(result);
+         }
+      }
+   }
+   
+}
