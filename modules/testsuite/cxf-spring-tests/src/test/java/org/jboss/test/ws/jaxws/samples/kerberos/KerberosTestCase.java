23756f6dc4645857674f4d0978662f93d42c90cb
jim.ma <jim.ma>
2013-05-14 10:55:44 +0000

    [JBWS-3634]:Add more tests

--- modules/testsuite/cxf-spring-tests/src/test/java/org/jboss/test/ws/jaxws/samples/kerberos/KerberosTestCase.java
+++ modules/testsuite/cxf-spring-tests/src/test/java/org/jboss/test/ws/jaxws/samples/kerberos/KerberosTestCase.java
@@ -87,41 +87,183 @@ import org.jboss.wsf.test.JBossWSTest;
 
 public class KerberosTestCase extends JBossWSTest
 {
-   private final String serviceURL = "http://" + getServerHost() + ":8080/jaxws-samples-wsse-kerberos/DoubleItKerberosSupport";
-   private final QName servicePort = new QName("http://www.example.org/contract/DoubleIt", "DoubleItKerberosSupportingPort");
-
+  
+   private static final String namespace = "http://www.example.org/contract/DoubleIt";
+   private static final QName serviceName = new QName(namespace, "DoubleItService");
    public static Test suite()
    {
       JBossWSCXFTestSetup testSetup;
       testSetup = new JBossWSCXFTestSetup(KerberosTestCase.class, "jaxws-samples-wsse-kerberos-client.jar jaxws-samples-wsse-kerberos.war");      
+      Map<String, String> sslOptions = new HashMap<String, String>();
+      if (isTargetJBoss7())
+      {
+         sslOptions.put("certificate-key-file", System.getProperty("org.jboss.ws.testsuite.server.keystore"));
+         sslOptions.put("password", "changeit");
+         sslOptions.put("verify-client", "false");
+         sslOptions.put("key-alias", "tomcat");
+      }
+      else
+      {
+         sslOptions.put("server-identity.ssl.keystore-path", System.getProperty("org.jboss.ws.testsuite.server.keystore"));
+         sslOptions.put("server-identity.ssl.keystore-password", "changeit");
+         sslOptions.put("server-identity.ssl.alias", "tomcat");
+      }
+      testSetup.setHttpsConnectorRequirement(sslOptions);
+      
       return testSetup;
    }
 
    public void testKerberosSupport() throws Exception
    {
-      QName serviceName = new QName("http://www.example.org/contract/DoubleIt", "DoubleItService");
+      String serviceURL = "http://" + getServerHost() + ":8080/jaxws-samples-wsse-kerberos/DoubleItKerberosSupport";
+      QName servicePort = new QName(namespace, "DoubleItKerberosSupportingPort");
+
+      QName serviceName = new QName(namespace, "DoubleItService");
       URL wsdlURL = new URL(serviceURL + "?wsdl");
       Service service = Service.create(wsdlURL, serviceName);
       DoubleItPortType proxy = (DoubleItPortType)service.getPort(servicePort, DoubleItPortType.class);
-      ((BindingProvider)proxy).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceURL);
-      setupWsse(proxy);
+      setupKerberosSupport(proxy);
       assertEquals(20, proxy.doubleIt(10));
    }
-
-
-   private void setupWsse(DoubleItPortType proxy)
+   
+   
+   public void testKerberosTransport() throws Exception
+   {
+      String serviceURL = "https://" + getServerHost() + ":8443/jaxws-samples-wsse-kerberos/DoubleItKerberosTransport";
+      QName servicePort = new QName(namespace, "DoubleItKerberosTransportPort");
+      QName serviceName = new QName(namespace, "DoubleItService");
+      URL wsdlURL = new URL(serviceURL + "?wsdl");
+      Service service = Service.create(wsdlURL, serviceName);
+      DoubleItPortType proxy = (DoubleItPortType)service.getPort(servicePort, DoubleItPortType.class);
+      setupKerberosTransport(proxy);
+      assertEquals(20, proxy.doubleIt(10));
+   }
+   
+   
+   public void testKerberosSymmetricSupporting() throws Exception
+   {
+      String serviceURL = "http://" + getServerHost() + ":8080/jaxws-samples-wsse-kerberos/DoubleItKerberosOverSymmetricSupporting";
+      QName servicePort = new QName(namespace, "DoubleItKerberosSymmetricSupportingPort");
+      QName serviceName = new QName(namespace, "DoubleItService");
+      URL wsdlURL = new URL(serviceURL + "?wsdl");
+      Service service = Service.create(wsdlURL, serviceName);
+      DoubleItPortType proxy = (DoubleItPortType)service.getPort(servicePort, DoubleItPortType.class);
+      setupSymmetricSupporting(proxy);
+      assertEquals(20, proxy.doubleIt(10));
+   }
+   
+   
+   public void testKerberosAsymmetric() throws Exception
+   {
+      String serviceURL = "http://" + getServerHost() + ":8080/jaxws-samples-wsse-kerberos/DoubleItKerberosAsymmetric";
+      QName servicePort = new QName(namespace, "DoubleItKerberosAsymmetricPort");
+      QName serviceName = new QName(namespace, "DoubleItService");
+      URL wsdlURL = new URL(serviceURL + "?wsdl");
+      Service service = Service.create(wsdlURL, serviceName);
+      DoubleItPortType proxy = (DoubleItPortType)service.getPort(servicePort, DoubleItPortType.class);
+      setupAsymmetric(proxy);
+      assertEquals(20, proxy.doubleIt(10));
+   }
+   
+   public void testKerberosOverAsymmetricSignedEncrypted() throws Exception
+   {
+      String serviceURL = "http://" + getServerHost() + ":8080/jaxws-samples-wsse-kerberos/DoubleItKerberosOverAsymmetricSignedEncrypted";
+      QName servicePort = new QName(namespace, "DoubleItKerberosAsymmetricSignedEncryptedPort");
+      QName serviceName = new QName(namespace, "DoubleItService");
+      URL wsdlURL = new URL(serviceURL + "?wsdl");
+      Service service = Service.create(wsdlURL, serviceName);
+      DoubleItPortType proxy = (DoubleItPortType)service.getPort(servicePort, DoubleItPortType.class);
+      setupAsymmetricSignedEncrypted(proxy);
+      assertEquals(20, proxy.doubleIt(10));
+   }
+   
+   public void testKerberosKerberosSymmetric() throws Exception
+   {
+      String serviceURL = "http://" + getServerHost() + ":8080/jaxws-samples-wsse-kerberos/DoubleItKerberosSymmetric";
+      QName servicePort = new QName(namespace, "DoubleItKerberosSymmetricPort");
+      QName serviceName = new QName(namespace, "DoubleItService");
+      URL wsdlURL = new URL(serviceURL + "?wsdl");
+      Service service = Service.create(wsdlURL, serviceName);
+      DoubleItPortType proxy = (DoubleItPortType)service.getPort(servicePort, DoubleItPortType.class);
+      setupSymmetricSupporting(proxy);
+      assertEquals(20, proxy.doubleIt(10));
+   }   
+   
+   private void setupAsymmetricSignedEncrypted(DoubleItPortType proxy)
    {
       Client client = ClientProxy.getClient(proxy);
       Endpoint cxfEndpoint = client.getEndpoint();
-      cxfEndpoint.put("ws-security.encryption.properties", Thread.currentThread().getContextClassLoader().getResource("META-INF/bob.properties"));
+      cxfEndpoint.put("ws-security.callback-handler","org.jboss.test.ws.jaxws.samples.wsse.kerberos.KeystorePasswordCallback");
+      cxfEndpoint.put("ws-security.encryption.properties", "META-INF/bob.properties");
       cxfEndpoint.put("ws-security.encryption.username", "bob");
+      cxfEndpoint.put("ws-security.signature.properties", "META-INF/alice.properties");
+      cxfEndpoint.put("ws-security.signature.username", "alice");
+      
+      client.getBus().getFeatures().add(new LoggingFeature());
+      client.getBus().getFeatures().add(new WSPolicyFeature());
+      KerberosClient kerberosClient = new KerberosClient(client.getBus());
+      kerberosClient.setServiceName("bob@service.ws.apache.org");
+      kerberosClient.setContextName("alice");
+      cxfEndpoint.put("ws-security.kerberos.client", kerberosClient);     
+   }
+
+  private void setupAsymmetric(DoubleItPortType proxy)
+   {
+      Client client = ClientProxy.getClient(proxy);
+      Endpoint cxfEndpoint = client.getEndpoint();
+      cxfEndpoint.put("ws-security.callback-handler","org.jboss.test.ws.jaxws.samples.wsse.kerberos.KeystorePasswordCallback");
+      cxfEndpoint.put("ws-security.encryption.properties", "META-INF/bob.properties");
+      cxfEndpoint.put("ws-security.encryption.username", "bob");
+      cxfEndpoint.put("ws-security.signature.properties", "META-INF/alice.properties");
+      cxfEndpoint.put("ws-security.signature.username", "alice");
+      
+      client.getBus().getFeatures().add(new LoggingFeature());
+      client.getBus().getFeatures().add(new WSPolicyFeature());
+      KerberosClient kerberosClient = new KerberosClient(client.getBus());
+      kerberosClient.setServiceName("bob@service.ws.apache.org");
+      kerberosClient.setContextName("alice");
+      cxfEndpoint.put("ws-security.kerberos.client", kerberosClient);     
+   }
+     
+   private void setupSymmetricSupporting(DoubleItPortType proxy)
+   {
+      Client client = ClientProxy.getClient(proxy);
+      Endpoint cxfEndpoint = client.getEndpoint();
+      cxfEndpoint.put("ws-security.encryption.properties", "META-INF/bob.properties");
+      cxfEndpoint.put("ws-security.encryption.username", "bob");
+      
+      client.getBus().getFeatures().add(new LoggingFeature());
+      client.getBus().getFeatures().add(new WSPolicyFeature());
+      KerberosClient kerberosClient = new KerberosClient(client.getBus());
+      kerberosClient.setServiceName("bob@service.ws.apache.org");
+      kerberosClient.setContextName("alice");
+      cxfEndpoint.put("ws-security.kerberos.client", kerberosClient);     
+   }
+
+   private void setupKerberosSupport(DoubleItPortType proxy)
+   {
+      Client client = ClientProxy.getClient(proxy);
+      Endpoint cxfEndpoint = client.getEndpoint();
+      client.getBus().getFeatures().add(new LoggingFeature());
+      client.getBus().getFeatures().add(new WSPolicyFeature());
+      KerberosClient kerberosClient = new KerberosClient(client.getBus());
+      kerberosClient.setServiceName("bob@service.ws.apache.org");
+      kerberosClient.setContextName("alice");
+      cxfEndpoint.put("ws-security.kerberos.client", kerberosClient);     
+   }
+   
+   
+   private void setupKerberosTransport(DoubleItPortType proxy)
+   {
+      Client client = ClientProxy.getClient(proxy);
+      Endpoint cxfEndpoint = client.getEndpoint();
+      cxfEndpoint.put("ws-security.is-bsp-compliant", "false");
  
       client.getBus().getFeatures().add(new LoggingFeature());
       client.getBus().getFeatures().add(new WSPolicyFeature());
       KerberosClient kerberosClient = new KerberosClient(client.getBus());
       kerberosClient.setServiceName("bob@service.ws.apache.org");
       kerberosClient.setContextName("alice");
-      cxfEndpoint.put("ws-security.kerberos.client", kerberosClient);
-      
+      cxfEndpoint.put("ws-security.kerberos.client", kerberosClient);     
    }
 }
