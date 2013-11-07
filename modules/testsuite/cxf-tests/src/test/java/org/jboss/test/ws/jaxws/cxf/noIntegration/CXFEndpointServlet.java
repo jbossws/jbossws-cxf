a31241aded1dcc23a7def11eae71d371a696e5af
asoldano <asoldano>
2013-11-07 22:29:26 +0000

    [JBWS-3729] ... but still use the bus that was created in cxf servlet' loadBus()

--- modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/noIntegration/CXFEndpointServlet.java
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/noIntegration/CXFEndpointServlet.java
@@ -24,6 +24,7 @@ package org.jboss.test.ws.jaxws.cxf.noIntegration;
 import javax.servlet.ServletConfig;
 import javax.xml.ws.Endpoint;
 
+import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
 
@@ -39,6 +40,9 @@ public class CXFEndpointServlet extends CXFNonSpringServlet
 
       // You could add the endpoint publish codes here
       try {
+         //be sure to use the bus that's been created in loadBus..
+         Bus bus = getBus();
+         BusFactory.setThreadDefaultBus(bus);
          Endpoint.publish("/Echo1", new EchoImpl());
       } finally {
          //free the thread default bus association in the current thread which
