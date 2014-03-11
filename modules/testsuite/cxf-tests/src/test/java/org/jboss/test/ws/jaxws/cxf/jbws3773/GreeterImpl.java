08e32e87f21f7e7e282b714713a7ecc678de1592
asoldano <asoldano>
2014-03-11 16:05:34 +0000

    [JBWS-3773] Adding testcase

---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/jbws3773/GreeterImpl.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.cxf.jbws3773;
+
+import javax.annotation.Resource;
+import javax.jws.WebService;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.soap.Addressing;
+
+@WebService(serviceName = "SOAPService", portName = "SoapPort", 
+            endpointInterface = "org.jboss.test.ws.jaxws.cxf.jbws3773.Greeter", 
+            targetNamespace = "http://jboss.org/hello_world")
+@Addressing
+public class GreeterImpl implements Greeter
+{
+   @Resource
+   private WebServiceContext webServiceCtx;
+   
+   public String sayHi(String request)
+   {
+      MessageContext ctx = webServiceCtx.getMessageContext();
+      HttpServletRequest hsr = (HttpServletRequest)ctx.get(MessageContext.SERVLET_REQUEST);
+      String scheme = hsr.getScheme();
+      return scheme;
+   }
+}
---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/jbws3773/JBWS3773TestCase.java
@@ -0,0 +1,87 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.cxf.jbws3773;
+
+import java.net.URL;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+import javax.xml.ws.soap.AddressingFeature;
+
+import junit.framework.Test;
+
+import org.apache.cxf.ws.addressing.AddressingProperties;
+import org.apache.cxf.ws.addressing.AttributedURIType;
+import org.apache.cxf.ws.addressing.EndpointReferenceType;
+import org.apache.cxf.ws.addressing.JAXWSAConstants;
+import org.apache.cxf.ws.addressing.impl.AddressingPropertiesImpl;
+import org.jboss.ws.common.IOUtils;
+import org.jboss.wsf.test.JBossWSTest;
+import org.jboss.wsf.test.JBossWSTestSetup;
+
+public class JBWS3773TestCase extends JBossWSTest
+{
+   public static Test suite()
+   {
+      return new JBossWSTestSetup(JBWS3773TestCase.class, "jaxws-cxf-jbws3773.war");
+   }
+
+   public void testServletRequestAvailability() throws Exception
+   {
+      Greeter greeter = initPort();
+
+      AddressingProperties addrProperties = new AddressingPropertiesImpl();
+
+      EndpointReferenceType replyTo = new EndpointReferenceType();
+      AttributedURIType replyToURI = new AttributedURIType();
+      replyToURI.setValue("http://" + getServerHost() + ":8080/jaxws-cxf-jbws3773/target/replyTo");
+      replyTo.setAddress(replyToURI);
+      addrProperties.setReplyTo(replyTo);
+
+      BindingProvider provider = (BindingProvider)greeter;
+      Map<String, Object> requestContext = provider.getRequestContext();
+      requestContext.put(JAXWSAConstants.CLIENT_ADDRESSING_PROPERTIES, addrProperties);
+
+      greeter.sayHi("Foo");
+      Thread.sleep(1500);
+      String result = getTargetServletResult();
+      assertTrue("Expected ReplyTo:", result.startsWith("ReplyTo:"));
+      assertTrue("Expected <return>http</return>:", result.indexOf("<return>http</return>") > 0);
+   }
+  
+   private Greeter initPort() throws Exception
+   {
+      URL wsdlURL = new URL("http://" + getServerHost() + ":8080/jaxws-cxf-jbws3773/SOAPService?wsdl");
+      QName qname = new QName("http://jboss.org/hello_world", "SOAPService");
+      Service service = Service.create(wsdlURL, qname);
+      Greeter greeter = service.getPort(Greeter.class, new AddressingFeature());
+      return greeter;
+   }
+
+   private String getTargetServletResult() throws Exception
+   {
+      URL url = new URL("http://" + getServerHost() + ":8080/jaxws-cxf-jbws3773/target/result");
+      return IOUtils.readAndCloseStream(url.openStream());
+   }
+}
