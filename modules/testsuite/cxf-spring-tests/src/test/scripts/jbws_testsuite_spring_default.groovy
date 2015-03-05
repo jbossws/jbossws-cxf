43d33f5c6c2e0812ab01b236be3e543ed91385d2
asoldano <asoldano>
2015-03-05 13:32:26 +0000

    [JBWS-3874] Removing tests from cxf-spring-tests module

--- modules/testsuite/cxf-spring-tests/src/test/scripts/jbws_testsuite_spring_default.groovy
+++ modules/testsuite/cxf-spring-tests/src/test/scripts/jbws_testsuite_spring_default.groovy
@@ -94,29 +94,6 @@ loginModuleDigest.appendNode('module-option', ['name':'usersProperties','value':
 loginModuleDigest.appendNode('module-option', ['name':'rolesProperties','value':project.properties['testResourcesDir'] + '/jaxws/samples/wsse/username-digest/WEB-INF/jbossws-roles.properties'])
 
 /**
- * Add a JMS queue like this
- *
- *  <subsystem xmlns="urn:jboss:domain:messaging:2.0">
- *      <hornetq-server>
- *          <jms-destinations>
- *              <jms-queue name="testQueue">
- *                  <entry name="queue/test"/>
- *                  <entry name="java:jboss/exported/jms/queue/test"/>
- *              </jms-queue>
- *          </jms-destinations>
- *      </hornetq-server>
- *  </subsystem>
- **/
-def hornetqServer = root.profile.subsystem.'hornetq-server'[0]
-def jmsDestinations = hornetqServer.'jms-destinations'[0]
-if (jmsDestinations == null) {
-    jmsDestinations = hornetqServer.appendNode('jms-destinations');
-}
-def jmsQueue = jmsDestinations.appendNode('jms-queue', ['name':'testQueue'])
-jmsQueue.appendNode('entry', ['name':'queue/test'])
-jmsQueue.appendNode('entry', ['name':'java:jboss/exported/jms/queue/test'])
-
-/**
  * Save the configuration to a new file
  */
 def writer = new StringWriter()
@@ -125,16 +102,3 @@ new XmlNodePrinter(new PrintWriter(writer)).print(root)
 def f = new File(project.properties['outputFile'])
 f.write(writer.toString())
 
-/*
- * copy the preconfigured application-roles.properties and application-users.properties
- * files into the standalone/configure directory
- */
-def srcUsersProperties = project.properties['srcUsersProperties']
-def destUsersProperties = project.properties['destUsersProperties']
-new AntBuilder().copy( file:srcUsersProperties,
-    tofile:destUsersProperties)
-
-def srcRolesProperties = project.properties['srcRolesProperties']
-def destRolesProperties = project.properties['destRolesProperties']
-new AntBuilder().copy( file:srcRolesProperties,
-    tofile:destRolesProperties)
