cb22189bff6370f0c57e611659c54b088738b942
asoldano <asoldano>
2015-03-30 15:11:57 +0000

    [JBWS-3900] Moving the check on bindingoperation out of the invocation try-finally block

--- modules/server/src/main/java/org/jboss/wsf/stack/cxf/JBossWSInvoker.java
+++ modules/server/src/main/java/org/jboss/wsf/stack/cxf/JBossWSInvoker.java
@@ -132,6 +132,11 @@ public class JBossWSInvoker extends JAXWSMethodInvoker implements Invoker
       {
          targetBean = this.getServiceObject(exchange);
       }
+      final Method method = (bop == null) ? null : md.getMethod(bop);
+      if (method == null)
+      {
+         throw Messages.MESSAGES.missingBindingOpeartionAndDispatchedMethod();
+      }
 
       //[JBWS-3843] workaround: set the CallbackHandler threadlocal again; as a matter of fact, if that's in the Exchange,
       //DIGEST auth is being used and that will cause the EJB layer to re-do authentication because of the bug
@@ -143,13 +148,8 @@ public class JBossWSInvoker extends JAXWSMethodInvoker implements Invoker
          {
             CallbackHandlerPolicyContextHandler.setCallbackHandler(cbHandler);
          }
-         Method method = bop == null ? null : md.getMethod(bop);
-         if (bop == null && method == null)
-         {
-            throw Messages.MESSAGES.missingBindingOpeartionAndDispatchedMethod();
-         }
          obj = invoke(exchange, targetBean,
-               adjustMethodAndParams(md.getMethod(bop), exchange, params, targetBean.getClass()), params);
+               adjustMethodAndParams(method, exchange, params, targetBean.getClass()), params);
       }
       finally
       {
