7ac1ddfcf53f76f440aba12d55d05cdf55a55df5
asoldano <asoldano>
2015-06-16 22:07:37 +0000

    [JBWS-3920] Default to OneWayProcessorInterceptor.USE_ORIGINAL_THREAD = true

--- modules/server/src/main/java/org/jboss/wsf/stack/cxf/interceptor/MessagePropertySettingInterceptor.java
+++ modules/server/src/main/java/org/jboss/wsf/stack/cxf/interceptor/MessagePropertySettingInterceptor.java
@@ -21,28 +21,20 @@
  */
 package org.jboss.wsf.stack.cxf.interceptor;
 
-import org.apache.cxf.interceptor.Fault;
-import org.apache.cxf.interceptor.OneWayProcessorInterceptor;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
 import org.apache.cxf.ws.addressing.soap.MAPCodec;
-import org.jboss.wsf.spi.deployment.Endpoint;
-import org.jboss.wsf.spi.deployment.EndpointType;
 
 /**
  * This interceptor adds the following context properties to cxf message:
  * <p>1.decoupled_fault_support<p>
  * It enables decoupled faultTo. This is an optional feature in cxf and we
  * need this to be default to make it same behavior with native stack.
- * <p>2.OneWayProcessorInterceptor.USE_ORIGINAL_THREAD<p>
- * This will force one way operation to use original thread for ejb webserivce endpoint
- * <p>to avoid authorization failure from ejb container 
  * @author <a href="mailto:ema@redhat.com">Jim Ma</a>
  */
 public class MessagePropertySettingInterceptor extends AbstractPhaseInterceptor<Message>
 {
-   private static EjbWSOneWayThreadInterceptor ejbOneWayInterceptor = new EjbWSOneWayThreadInterceptor();
 
    public MessagePropertySettingInterceptor()
    {
@@ -53,38 +45,10 @@ public class MessagePropertySettingInterceptor extends AbstractPhaseInterceptor<
    public void handleMessage(Message message)
    {
       message.put("org.apache.cxf.ws.addressing.decoupled_fault_support", true);
-      message.getInterceptorChain().add(ejbOneWayInterceptor);
    }
 
    public void handleFault(Message message)
    {
       //complete
    }
-
-   private static class EjbWSOneWayThreadInterceptor extends AbstractPhaseInterceptor<Message>
-   {
-      public EjbWSOneWayThreadInterceptor()
-      {
-         super(Phase.PRE_LOGICAL);
-         this.addBefore(OneWayProcessorInterceptor.class.getName());
-      }
-
-      @Override
-      public void handleMessage(Message message) throws Fault
-      {
-
-         Endpoint endpoint = message.getExchange().get(Endpoint.class);
-         if (endpoint == null)
-         {
-            return;
-         }
-         //Use original thread for oneway message to avoid authorization failure in ejb container for webservice endpoint 
-         if (endpoint.getType() == EndpointType.JAXWS_EJB3 && message.getExchange().isOneWay() && !isRequestor(message))
-         {
-            message.put(OneWayProcessorInterceptor.USE_ORIGINAL_THREAD, true);
-         }
-
-      }
-   }
-
 }
