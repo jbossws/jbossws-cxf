2378c9076e6a715ac9277b5c33b68b3f302c41ad
Alessio Soldano <asoldano@redhat.com>
2015-09-15 01:12:31 +0200

    [JBWS-3947] Fix logs and add a bit of iterations... ;-)

--- modules/testsuite/perf-tests/src/test/scripts/perf.groovy
+++ modules/testsuite/perf-tests/src/test/scripts/perf.groovy
@@ -1,7 +1,10 @@
 def root = new XmlParser().parse(project.properties['inputFile'])
 
 /**
- * Fix logging: remove CONSOLE handler and set a specific log file
+ * Fix logging:
+ * - remove CONSOLE handler and set a specific log file
+ * - set ERROR level for root-logger
+ * - add specfic categories with level set to ERROR for cxf and jboss
  *
  */
 def logHandlers = root.profile.subsystem.'root-logger'.handlers[0]
@@ -10,6 +13,13 @@ logHandlers.remove(consoleHandler)
 def file = root.profile.subsystem.'periodic-rotating-file-handler'.file[0]
 file.attributes()['path'] = project.properties['serverLog']
 
+def rootLogger = root.profile.subsystem.'root-logger'[0]
+rootLogger.level.@name = 'ERROR'
+def logSubsystem = rootLogger.parent()
+def jbossCategory = logSubsystem.appendNode('logger', ['category':'org.jboss'])
+jbossCategory.appendNode('level', ['name':'ERROR'])
+def apacheCategory = logSubsystem.appendNode('logger', ['category':'org.apache'])
+apacheCategory.appendNode('level', ['name':'ERROR'])
 
 /**
  * Save the configuration to a new file
