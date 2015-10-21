baf593f336c9f29133cd799783cc00a11e21bca3
Jim Ma <ema@redhat.com>
2015-10-21 13:39:56 +0800

    [JBWS-3956]:Add deployment order and dependency

--- modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsrm/store/WSRMStoreFeatureTestCase.java
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsrm/store/WSRMStoreFeatureTestCase.java
@@ -29,11 +29,13 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.wsf.test.JBossWSTest;
 import org.jboss.wsf.test.JBossWSTestHelper;
@@ -51,16 +53,28 @@ public class WSRMStoreFeatureTestCase extends JBossWSTest
    private URL baseURL;
    @ArquillianResource
    private InitialContext ctx;
+   private static final String DEP1 = "DsDeployment";
+   private static final String DEP2 = "RMStoreDeployment";
    
-   @Deployment(testable = false)
+   
+   @Deployment(name=DEP1, testable = false, order=1)
+   public static JavaArchive createDsDeployment()
+   {
+	  JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "rmstore-ds.jar");
+      archive.addAsManifestResource(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/samples/wsrm-store/rmstore-ds.xml"), "rmstore-ds.xml");
+      return archive;
+   }
+   
+   
+   @Deployment(name=DEP2, testable = false, order=2)
    public static WebArchive createDeployment()
    {
       WebArchive archive = ShrinkWrap.create(WebArchive.class, "jaxws-samples-wsrm-store.war");
       archive.addAsManifestResource(new StringAsset("Manifest-Version: 1.0\n" + "Dependencies: org.jboss.ws.common, org.apache.cxf.impl\n"), "MANIFEST.MF")
-            .addAsManifestResource(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/samples/wsrm-store/rmstore-ds.xml"), "rmstore-ds.xml")
             .addClass(org.jboss.test.ws.jaxws.samples.wsrm.store.Endpoint.class).addClass(org.jboss.test.ws.jaxws.samples.wsrm.store.EndpointImpl.class)
             .addClass(org.jboss.test.ws.jaxws.samples.wsrm.store.RMStoreFeature.class)
             .addClass(org.jboss.test.ws.jaxws.samples.wsrm.store.RMStoreCheckInterceptor.class)
+            .addAsWebInfResource(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/samples/wsrm-store/WEB-INF/jboss-deployment-structure.xml"))
             .setWebXML(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/samples/wsrm-store/WEB-INF/web.xml"))
             .addAsManifestResource(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/samples/wsrm-store/WEB-INF/permissions.xml"), "permissions.xml");
       JBossWSTestHelper.writeToFile(archive);
@@ -69,6 +83,7 @@ public class WSRMStoreFeatureTestCase extends JBossWSTest
    
    @Test
    @RunAsClient
+   @OperateOnDeployment(DEP2)
    public void test() throws Exception
    {  
       QName serviceName = new QName("http://www.jboss.org/jbossws/ws-extensions/wsrm", "RMService");
