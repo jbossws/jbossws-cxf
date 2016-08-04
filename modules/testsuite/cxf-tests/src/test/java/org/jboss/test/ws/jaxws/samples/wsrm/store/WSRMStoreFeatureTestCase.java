d3021aa4a59a79c2c0fef3a40ff01cb9f9ce4266
Alessio Soldano <asoldano@redhat.com>
2016-08-04 20:34:59 +0200

    Disabling test because of CXF-6994

--- modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsrm/store/WSRMStoreFeatureTestCase.java
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/samples/wsrm/store/WSRMStoreFeatureTestCase.java
@@ -39,6 +39,7 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.wsf.test.JBossWSTest;
 import org.jboss.wsf.test.JBossWSTestHelper;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -47,6 +48,7 @@ import org.junit.runner.RunWith;
  * @author <a herf="mailto:ema@redhat.com">Jim Ma</a>
  *
  */
+@Ignore("[CXF-6994] RMCaptureInInterceptor running on GET requests")
 public class WSRMStoreFeatureTestCase extends JBossWSTest
 {
    @ArquillianResource
