96cbbe18a89a8eb820f9b0552b7a7816de1330b0
Alessio Soldano <soldano.servizi@email.it>
2018-04-04 23:34:06 +0200

    [JBWS-4091] Travis revised to support wfly master (#52)
    * Travis revised to support wfly12 bld
    
    * Revisiting parameters and targets a bit
    
    * Misc additional optimizations

---
+++ travis-scripts/jbossws-test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ev
+
+
+# uncomment to debug on non zero exit; be sure to print the correct log file
+#function finish {
+#  if [[ $USE_WFLY_MASTER = "true" ]]; then
+#     WFLY_TARGET=`pwd`"/wfly/wildfly/dist/target/"
+#     WFLY_HOME=$(find $WFLY_TARGET -name \wildfly\* -type d -maxdepth 1 -print | head -n1)
+#     echo "############ ERROR  dumping server.log #####################"
+#     cat $WFLY_HOME"/standalone/log/server.log"
+#  fi
+#}
+#
+#trap finish EXIT
+
+
+SERVER_VERSION=$1
+SECURITY_MGR=$2
+USE_WFLY_MASTER=$3
+
+D_SERVER_HOME=""
+
+if [[ $USE_WFLY_MASTER = "true" ]]; then
+
+  MYPWD=`pwd`
+
+  rm -rf wfly
+  mkdir wfly
+  cd wfly
+  git clone https://github.com/wildfly/wildfly.git
+  cd wildfly
+
+  # compile in silence.  The bld output is too much for travis log
+  mvn --quiet clean install -DskipTests -Denforcer.skip -Dcheckstyle.skip
+
+  WFLY_TARGET=`pwd`"/dist/target/"
+  WFLY_HOME=$(find $WFLY_TARGET -maxdepth 1 -name \wildfly\* -type d -print | head -n1)
+  D_SERVER_HOME="-Dserver.home="$WFLY_HOME
+  cd $MYPWD
+fi
+
+mvn -s .travis-settings.xml -B -fae -DSECMGR=${SECURITY_MGR} -P${SERVER_VERSION} ${D_SERVER_HOME} integration-test
