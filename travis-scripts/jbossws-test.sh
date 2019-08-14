7e35476a12d4b952b1f423953a3f1cb9d636868f
Jan Bliznak <jbliznak@redhat.com>
2019-08-14 16:04:13 +0200

    [JBWS-4183] Test elytron configuration in travis

--- travis-scripts/jbossws-test.sh
+++ travis-scripts/jbossws-test.sh
@@ -3,4 +3,5 @@
 set -ex
 
 SERVER_VERSION=$1
-mvn -s .travis-settings.xml -B -V -fae -P${SERVER_VERSION} verify
+ELYTRON=$2
+mvn -s .travis-settings.xml -B -V -fae verify -P${SERVER_VERSION} ${ELYTRON:+-Delytron}
