c9c74de24d8f834413f2d6b165741f23850a6247
jbliznak <jbliznak@users.noreply.github.com>
2019-11-12 04:16:47 +0100

    [JBWS-4183][JBWS-4184] Travis configuration changes and simple elytron profile activation (#117)
    * [JBWS-4184] Remove security manager matrix combination from travis
    
    * [JBWS-4183] Split conditional WF build and tests execution so that separate timeouts could be used
    - also introduce some tweaks to fix and speed up the execution
    
    * [JBWS-4183] Ensure Elytron-enabled testing is activated predictably
    
    - one property was defined in more profiles thus it was up to Maven
      current implementation of profiles execution order;
      this was error-prone so this weakness was removed by setting
      the property in one profile only and now the profiles can be activated
      in arbitrary order
    - as the Elytron profile is relevant only to testing, it was moved to
      modules/testsuite/pom.xml
    - also option to activate profile via system property was added

--- travis-scripts/jbossws-test.sh
+++ travis-scripts/jbossws-test.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
 
-set -ev
-java -version
-
-# uncomment to debug on non zero exit; be sure to print the correct log file
-#function finish {
-#  if [[ $USE_WFLY_MASTER = "true" ]]; then
-#     WFLY_TARGET=`pwd`"/wfly/wildfly/dist/target/"
-#     WFLY_HOME=$(find $WFLY_TARGET -name \wildfly\* -type d -maxdepth 1 -print | head -n1)
-#     echo "############ ERROR  dumping server.log #####################"
-#     cat $WFLY_HOME"/standalone/log/server.log"
-#  fi
-#}
-#
-#trap finish EXIT
-
+set -ex
 
 SERVER_VERSION=$1
-SECURITY_MGR=$2
-USE_WFLY_MASTER=$3
-if [[ $USE_WFLY_MASTER = "true" ]]; then
-
-  MYPWD=`pwd`
-
-  rm -rf wfly
-  mkdir wfly
-  cd wfly
-  git clone https://github.com/wildfly/wildfly.git
-  cd wildfly
-
-  # compile in silence.  The bld output is too much for travis log
-  mvn --quiet clean install -DskipTests -Denforcer.skip -Dcheckstyle.skip -Prelease
-  cd $MYPWD
-fi
-mvn -s .travis-settings.xml -B -fae -DSECMGR=${SECURITY_MGR} -P${SERVER_VERSION} integration-test
-
+mvn -s .travis-settings.xml -B -V -fae -P${SERVER_VERSION} verify
