bae0552f9d8222273f52de6203ab8d0b319f1ecd
Jim Ma <ema@redhat.com>
2021-09-29 11:10:07 +0800

    [JBWS-4230]:More groovy scripts for test settings when run against WFLY25

--- modules/testsuite/cxf-tests/src/test/scripts/jbws-testsuite-default-config-tests-nolegacy.groovy
+++ modules/testsuite/cxf-tests/src/test/scripts/jbws-testsuite-default-config-tests-nolegacy.groovy
@@ -83,10 +83,7 @@ def mechanismRealm=mechanism.appendNode('mechanism-realm',['realm-name':'JBossWS
 //add to undertow
 def undertowSubsystem = getSubsystem(root, "urn:jboss:domain:undertow:")
 
-//TODO: is there better create node as sibling in groovy
-def undertowChildren = undertowSubsystem.children()
-def undertowAppSecurityDomains = new groovy.util.Node(null, 'application-security-domains', [])
-undertowChildren.add(undertowAppSecurityDomains)
+def undertowAppSecurityDomains = undertowSubsystem.'application-security-domains'[0]
 def appSecurityDomain = undertowAppSecurityDomains.appendNode('application-security-domain', ['name':'JBossWS','http-authentication-factory':'JBossWS'])
 
 /**
