9334492d457397a9b5ba06e0fb4cd413328252fc
Jim Ma <ema@redhat.com>
2023-09-08 09:24:25 +0800

    [JBWS-4383]:Improve the common utility to check the WFLY readiness in cloud test

--- modules/testsuite/cloud-tests/k8s/common/src/main/java/org/jboss/ws/cloud/test/JBossWSKubernetesTest.java
+++ modules/testsuite/cloud-tests/k8s/common/src/main/java/org/jboss/ws/cloud/test/JBossWSKubernetesTest.java
@@ -18,6 +18,8 @@
  */
 package org.jboss.ws.cloud.test;
 
+import io.fabric8.kubernetes.api.model.ServicePort;
+import io.fabric8.kubernetes.api.model.ServiceSpec;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.LocalPortForward;
 import io.restassured.RestAssured;
@@ -25,52 +27,64 @@ import io.restassured.path.json.JsonPath;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class JBossWSKubernetesTest implements JBossWSServerContainer {
+public class JBossWSKubernetesTest {
+
     @InjectKubeClient
     private KubernetesClient jbossWSKubernetesTestKubeClient;
 
     @BeforeEach
     public void checkServerReady() {
-        waitWFLYReady(this.jbossWSKubernetesTestKubeClient, this.getContainerName(), 60000);
+        waitWFLYReady(this.jbossWSKubernetesTestKubeClient, 30000);
     }
-    public static boolean waitWFLYReady(KubernetesClient k8sClient, String containerName, long timeout) {
-        LocalPortForward p = k8sClient.services().withName(containerName).portForward(9990);
-        assertTrue(p.isAlive());
-        URL url = null;
-        try {
-            url = new URL("http://localhost:" + p.getLocalPort() + "/health/ready");
-        } catch (MalformedURLException e) {
-            //
-        }
-        long startTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() - startTime < timeout) {
+    public static boolean waitWFLYReady(KubernetesClient k8sClient, long timeout) {
+        AtomicBoolean ready = new AtomicBoolean(false);
+        k8sClient.services().resources().filter(serviceResource ->
+                   serviceResource.get().getSpec().getPorts().stream().anyMatch(
+                   servicePort -> servicePort.getTargetPort().getIntVal() == 9990)
+        ).forEach(serviceResource -> {
+            ServicePort servicePort = serviceResource.get().getSpec().getPorts().stream().filter(port -> port.getTargetPort().getIntVal() == 9990).findFirst().get();
+            LocalPortForward p = serviceResource.portForward(servicePort.getPort());
+            assertTrue(p.isAlive());
+            URL url = null;
             try {
-                //This is a workaround for restassured print out some warning message, we try to openstream before check response with restassured
-                //INFO: I/O exception (org.apache.http.NoHttpResponseException) caught when processing request to {}->http://localhost:53380:
-                //The target server failed to respond
-                url.openStream();
-                io.restassured.response.Response response = RestAssured.given().get(url);
-                if (response.getStatusCode() == 200) {
-                    JsonPath jsonPath = response.jsonPath();
-                    String readyStatus = jsonPath.getString("status");
-                    if (readyStatus.equalsIgnoreCase("UP")) {
-                        return true;
+                url = new URL("http://localhost:" + p.getLocalPort() + "/health/ready");
+            } catch (MalformedURLException e) {
+                //
+            }
+            long startTime = System.currentTimeMillis();
+            while (System.currentTimeMillis() - startTime < timeout) {
+                try {
+                    //This is a workaround for restassured print out some warning message, we try to openstream before check response with restassured
+                    //INFO: I/O exception (org.apache.http.NoHttpResponseException) caught when processing request to {}->http://localhost:53380:
+                    //The target server failed to respond
+                    url.openStream();
+                    io.restassured.response.Response response = RestAssured.given().get(url);
+                    if (response.getStatusCode() == 200) {
+                        JsonPath jsonPath = response.jsonPath();
+                        String readyStatus = jsonPath.getString("status");
+                        if (readyStatus.equalsIgnoreCase("UP")) {
+                            ready.set(true);
+                            return;
+                        }
                     }
+                } catch (IOException e) {
+                    //the WFLY is not ready.
                 }
-            } catch (IOException e) {
-                //the WFLY is not ready.
+                try {
+                    Thread.sleep(timeout / 5);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
             }
-            try {
-                Thread.sleep(timeout/5);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                break;
-            }
-        }
-        return false;
+            ready.set(false);
+        });
+        //There is no WFLY instance running or the WFLY management port isn't exposed in service
+        return ready.get();
     }
 }
