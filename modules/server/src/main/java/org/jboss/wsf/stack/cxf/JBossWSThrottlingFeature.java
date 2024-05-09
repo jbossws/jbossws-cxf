4f89c3b7213661db8dc6839a9763ab542303c5f1
Jim Ma <ema@redhat.com>
2024-05-09 11:34:02 +0800

    [JBWS-3912]:Add throttling features support and test case

---
+++ modules/server/src/main/java/org/jboss/wsf/stack/cxf/JBossWSThrottlingFeature.java
@@ -0,0 +1,45 @@
+package org.jboss.wsf.stack.cxf;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.feature.AbstractPortableFeature;
+import org.apache.cxf.feature.DelegatingFeature;
+import org.apache.cxf.interceptor.InterceptorProvider;
+import org.apache.cxf.throttling.ThrottlingInterceptor;
+import org.apache.cxf.throttling.ThrottlingManager;
+import org.apache.cxf.throttling.ThrottlingResponseInterceptor;
+
+public class JBossWSThrottlingFeature extends DelegatingFeature<JBossWSThrottlingFeature.Portable> {
+    Portable portable = null;
+    public JBossWSThrottlingFeature() {
+        super(new JBossWSThrottlingFeature.Portable());
+    }
+
+    public void setThrottlingManager(ThrottlingManager manager) {
+        this.getDelegate().setThrottlingManager(manager);
+    }
+
+    public static class Portable implements AbstractPortableFeature {
+
+        ThrottlingManager manager = null;
+        public Portable() {
+        }
+
+        public void setThrottlingManager(ThrottlingManager manager) {
+            this.manager = manager;
+        }
+
+        @Override
+        public void doInitializeProvider(InterceptorProvider provider, Bus bus) {
+            ThrottlingManager m = manager;
+
+            if (m == null) {
+                throw new IllegalArgumentException("ThrottlingManager must not be null");
+            }
+            for (String p : m.getDecisionPhases()) {
+                provider.getInInterceptors().add(new ThrottlingInterceptor(p, m));
+            }
+            provider.getOutInterceptors().add(new ThrottlingResponseInterceptor());
+            provider.getOutFaultInterceptors().add(new ThrottlingResponseInterceptor());
+        }
+    }
+}
---
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/throttling/ThrottlingTestCase.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jboss.test.ws.jaxws.cxf.throttling;
+
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.ws.BindingProvider;
+import jakarta.xml.ws.Service;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.namespace.QName;
+import org.apache.cxf.BusFactory;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.FileAsset;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.wsf.stack.cxf.client.Constants;
+import org.jboss.wsf.test.JBossWSTest;
+import org.jboss.wsf.test.JBossWSTestHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(ArquillianExtension.class)
+public class ThrottlingTestCase extends JBossWSTest {
+    @ArquillianResource
+    private URL baseURL;
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        WebArchive archive = ShrinkWrap.create(WebArchive.class, "jaxws-cxf-throttling.war")
+                .addClass(HelloWorld.class)
+                .addClass(HelloWorldImpl.class)
+                .setWebXML(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/cxf/throttling/WEB-INF/web.xml"))
+                .add(new FileAsset(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/cxf/throttling/WEB-INF/jaxws-endpoint-config.xml")), "jaxws-endpoint-config.xml");
+        return archive;
+    }
+
+
+    @Test
+    @RunAsClient
+    public void testThrottling() throws Exception {
+        //Throttling feature only allows 5 invocations and getPort access wsdl already
+        //called for once.
+        HelloWorld port = getPort();
+        for (int i = 0; i < 4; i++) {
+            String response = port.echo("hello");
+            Assertions.assertEquals("hello", response, "hello is expected and i:" + i);
+        }
+
+        try {
+            String response = port.echo("hello");
+            fail("Exception not thrown");
+        } catch (jakarta.xml.ws.WebServiceException e) {
+            Assertions.assertEquals(((BindingProvider)port).getResponseContext().get("jakarta.xml.ws.http.response.code"), 429);
+        }
+
+
+    }
+
+    private HelloWorld getPort() throws MalformedURLException
+    {
+        URL wsdlURL = new URL(baseURL.toString() + "/jaxws-cxf-throttling?wsdl");
+        QName serviceName = new QName("http://org.jboss.ws/jaxws/cxf/throttling", "HelloWorldService");
+        Service service = Service.create(wsdlURL, serviceName);
+        QName portQName = new QName("http://org.jboss.ws/jaxws/cxf/throttling", "HelloWorldImplPort");
+        HelloWorld port = (HelloWorld) service.getPort(portQName, HelloWorld.class);
+        return port;
+    }
+}
