0da99a402125ce5363146361d8b7cda0aad73000
Jim Ma <ema@redhat.com>
2024-06-21 10:35:45 +0800

    [JBWS-4421]:Move the RateLimitThorttlingManager to server module and improve the test

--- modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/throttling/ThrottlingTestCase.java
+++ modules/testsuite/cxf-tests/src/test/java/org/jboss/test/ws/jaxws/cxf/throttling/ThrottlingTestCase.java
@@ -18,26 +18,19 @@
  */
 package org.jboss.test.ws.jaxws.cxf.throttling;
 
-import jakarta.xml.soap.SOAPException;
 import jakarta.xml.ws.BindingProvider;
 import jakarta.xml.ws.Service;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.PrintStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.xml.namespace.QName;
-import org.apache.cxf.BusFactory;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.FileAsset;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
-import org.jboss.shrinkwrap.api.exporter.ZipExporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.wsf.stack.cxf.client.Constants;
 import org.jboss.wsf.test.JBossWSTest;
 import org.jboss.wsf.test.JBossWSTestHelper;
 import org.junit.jupiter.api.Assertions;
@@ -52,13 +45,10 @@ public class ThrottlingTestCase extends JBossWSTest {
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
         WebArchive archive = ShrinkWrap.create(WebArchive.class, "jaxws-cxf-throttling.war")
-                .setManifest(new StringAsset("Manifest-Version: 1.0\n"
-                        + "Dependencies: org.apache.cxf.impl\n"))
                 .addClass(HelloWorld.class)
                 .addClass(HelloWorldImpl.class)
                 .addClass(Hello.class)
                 .addClass(HelloImpl.class)
-                .addClass(RateLimitThorttlingManager.class)
                 .setWebXML(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/cxf/throttling/WEB-INF/web.xml"))
                 .add(new FileAsset(new File(JBossWSTestHelper.getTestResourcesDir() + "/jaxws/cxf/throttling/WEB-INF/jaxws-endpoint-config.xml")), "jaxws-endpoint-config.xml");
         return archive;
@@ -94,7 +84,7 @@ public class ThrottlingTestCase extends JBossWSTest {
         }
 
         try {
-            String res = port.sayHello("hello");
+            String res = port.sayHello("error");
             fail("Exception not thrown");
         } catch (jakarta.xml.ws.WebServiceException e) {
             Assertions.assertEquals(((BindingProvider)port).getResponseContext().get("jakarta.xml.ws.http.response.code"), 429);
