4496cbf1539610696050481cbf698fc702283226
Richard Opálka <opalka.richard@gmail.com>
2025-11-15 18:49:53 +0100

    [JBWS-4456] Remove Security Manager and Security Policy usages (preparation for JDK25 support)

--- modules/testsuite/shared-tests/src/test/java/org/jboss/test/ws/jaxws/smoke/tools/WSConsumeScriptTestCase.java
+++ modules/testsuite/shared-tests/src/test/java/org/jboss/test/ws/jaxws/smoke/tools/WSConsumeScriptTestCase.java
@@ -52,16 +52,7 @@ public class WSConsumeScriptTestCase extends org.jboss.test.ws.jaxws.smoke.tools
       String absOutput = new File(TEST_DIR, "wsconsume" + FS + "java").getAbsolutePath();
       String command = JBOSS_HOME + FS + "bin" + FS + "wsconsume" + EXT + " -v -k -o " + absOutput + " " + absWsdlLoc;
 
-      // wildfly9 security manager flag changed from -Djava.security.manager to -secmgr.
-      // Can't pass -secmgr arg through arquillian because it breaks arquillian's
-      // config of our tests.
-      // the -secmgr flag MUST be provided as an input arg to jboss-modules so it must
-      // come after the jboss-modules.jar ref.
-      String additionalJVMArgs = System.getProperty("additionalJvmArgs", "");
-      String securityManagerDesignator = additionalJVMArgs.replace("-Djava.security.manager", "-secmgr");
-
       Map<String, String> env = new HashMap<>();
-      env.put("JAVA_OPTS", securityManagerDesignator);
       executeCommand(command, null, "wsconsume", env);
       File javaSource = new File(TEST_DIR, "wsconsume" + FS + "java" + FS + "org" + FS + "openuri" + FS + "_2004" + FS + "_04" + FS + "helloworld" + FS + "EndpointInterface.java");
       assertTrue(javaSource.exists(), "Service endpoint interface not generated");
